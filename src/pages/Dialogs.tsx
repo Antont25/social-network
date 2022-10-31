2b80fc9484a0c705dd19139ad76f69d097f6cb4b
Antont25 <toxafootbol1994@gmail.com>
2022-10-31 17:48:57 +0200

    fix: auto scroll chat

--- src/pages/Dialogs.tsx
+++ src/pages/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState, UIEvent} from 'react';
 import style from '../components/Dialogs/dialogs.module.css'
 import Message from '../components/Dialogs/Messages/Message';
 import {DialogItems} from '../components/Dialogs/DialogItems';
@@ -9,54 +9,40 @@ import {useAppDispatch, useAppSelector} from '../common/utils/hooks/hooks';
 
 
 const Dialogs = () => {
+    const messagesEndRef = useRef<HTMLDivElement | null>(null)
+    const [isScroll, setIsScroll] = useState(true)
+
     const dispatch = useAppDispatch()
     const dialogs = useAppSelector(state => state.dialogsPage.dialogs)
     const messages = useAppSelector(state => state.dialogsPage.messages)
 
-    // const [messages, setMessage] = useState<MessageType[]>([])
-    // const [ws, setWs] = useState<WebSocket>()
-
     const clickAddNewMessages = (value: string) => {
         dispatch(addMessage(value))
     }
+    const scrollToBottom = () => {
+        if (isScroll) {
+            messagesEndRef.current?.scrollIntoView({behavior: 'smooth'})
+        }
+    }
+
+    const onScrollHandler = (e: UIEvent<HTMLElement>) => {
+        const maxScrollPosition = e.currentTarget.scrollHeight - e.currentTarget.clientHeight
+        if (Math.abs(maxScrollPosition - e.currentTarget.scrollTop) > 5) {
+            setIsScroll(false)
+        } else {
+            setIsScroll(true)
+        }
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages]);
+
     useEffect(() => {
         dispatch(createChatWS())
         return () => dispatch(removeChatWS())
     }, [])
 
-    // const closeHandler = (e: WebSocketEventMap['close']) => {
-    //     console.log('closeHandler')
-    //     clearInterval(closeID)
-    //     closeID = setInterval(() => createWS(), 3000)
-    //
-    // }
-
-    // const openHandler = () => {
-    //     setMessage([])
-    //     clearInterval(closeID)
-    // }
-
-
-    //     const createWS = async () => {
-    //         // const ws = await new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-    //
-    //         setWs(ws)
-    //
-    //         ws.addEventListener('message', messageHandler)
-    //         ws.addEventListener('close', closeHandler)
-    //         ws.addEventListener('open', openHandler)
-    //     }
-    //
-    //     createWS()
-    //
-    //     return () => {
-    //         clearInterval(closeID)
-    //         ws?.removeEventListener('message', messageHandler)
-    //         ws?.removeEventListener('close', closeHandler)
-    //         ws?.close()
-    //     }
-    //
-    // }, [])
 
     return (
         <div className={style.dialogs}>
@@ -65,13 +51,13 @@ const Dialogs = () => {
 
             </ul>
             <div className={style.message}>
-                <ul className={style.dialogItem}>
+                <ul className={style.dialogItem} onScroll={onScrollHandler}>
                     {messages.length > 0 && messages.map((item, index) => <Message key={index}
                                                                                    message={item.message}
                                                                                    photo={item.photo}
                                                                                    userId={item.userId}
                                                                                    userName={item.userName}/>)}
-
+                    <div ref={messagesEndRef}/>
                 </ul>
                 <div className={style.addMessage}>
                     <Textarea callback={clickAddNewMessages}
