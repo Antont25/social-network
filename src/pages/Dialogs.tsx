01bf9c8d2f471d17dc741e5aaeb2851f5d39c1d6
Antont25 <toxafootbol1994@gmail.com>
2022-11-03 12:05:08 +0200

    test: add userThunkFetchUnFollow.test.ts

--- src/pages/Dialogs.tsx
+++ src/pages/Dialogs.tsx
@@ -1,73 +1,73 @@
-import React, {useEffect, useRef, useState, UIEvent} from 'react';
-import style from '../components/Dialogs/dialogs.module.css'
-import Message from '../components/Dialogs/Messages/Message';
-import {DialogItems} from '../components/Dialogs/DialogItems';
-import {Textarea} from '../common/components/Textarea/Textarea';
-import {addMessage, createChatWS, removeChatWS} from '../redux/dialogsReducer';
-import {validationPostAndDialog} from '../common/utils/validation/validation';
-import {useAppDispatch, useAppSelector} from '../common/utils/hooks/hooks';
+import React, {useEffect, useRef, useState, UIEvent} from "react";
+import style from "../components/Dialogs/dialogs.module.css"
+import Message from "../components/Dialogs/Messages/Message";
+import {DialogItems} from "../components/Dialogs/DialogItems";
+import {Textarea} from "../common/components/Textarea/Textarea";
+import {addMessage, createChatWS, removeChatWS} from "../redux/dialogsReducer";
+import {validationPostAndDialog} from "../common/utils/validation/validation";
+import {useAppDispatch, useAppSelector} from "../common/utils/hooks/hooks";
 
 
 const Dialogs = () => {
-    const messagesEndRef = useRef<HTMLDivElement | null>(null)
-    const [isScroll, setIsScroll] = useState(true)
+  const messagesEndRef = useRef<HTMLDivElement | null>(null)
+  const [isScroll, setIsScroll] = useState(true)
 
-    const dispatch = useAppDispatch()
-    const dialogs = useAppSelector(state => state.dialogsPage.dialogs)
-    const messages = useAppSelector(state => state.dialogsPage.messages)
+  const dispatch = useAppDispatch()
+  const dialogs = useAppSelector(state => state.dialogsPage.dialogs)
+  const messages = useAppSelector(state => state.dialogsPage.messages)
 
-    const clickAddNewMessages = (value: string) => {
-        dispatch(addMessage(value))
+  const clickAddNewMessages = ( value: string ) => {
+    dispatch(addMessage(value))
+  }
+  const scrollToBottom = () => {
+    if (isScroll) {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
     }
-    const scrollToBottom = () => {
-        if (isScroll) {
-            messagesEndRef.current?.scrollIntoView({behavior: 'smooth'})
-        }
+  }
+
+  const onScrollHandler = ( e: UIEvent<HTMLElement> ) => {
+    const maxScrollPosition = e.currentTarget.scrollHeight - e.currentTarget.clientHeight
+    if (Math.abs(maxScrollPosition - e.currentTarget.scrollTop) > 5) {
+      setIsScroll(false)
+    } else {
+      setIsScroll(true)
     }
+  }
 
-    const onScrollHandler = (e: UIEvent<HTMLElement>) => {
-        const maxScrollPosition = e.currentTarget.scrollHeight - e.currentTarget.clientHeight
-        if (Math.abs(maxScrollPosition - e.currentTarget.scrollTop) > 5) {
-            setIsScroll(false)
-        } else {
-            setIsScroll(true)
-        }
-    }
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages]);
 
-    useEffect(() => {
-        scrollToBottom()
-    }, [messages]);
-
-    useEffect(() => {
-        dispatch(createChatWS())
-        return () => dispatch(removeChatWS())
-    }, [])
+  useEffect(() => {
+    dispatch(createChatWS())
+    return () => dispatch(removeChatWS())
+  }, [])
 
 
-    return (
-        <div className={style.dialogs}>
-            <ul>
-                {dialogs.map(item => <DialogItems key={item.id} name={item.name} id={`${item.id}`}/>)}
+  return (
+    <div className={style.dialogs}>
+      <ul>
+        {dialogs.map(item => <DialogItems key={item.id} name={item.name} id={`${item.id}`}/>)}
 
-            </ul>
-            <div className={style.message}>
-                <ul className={style.dialogItem} onScroll={onScrollHandler}>
-                    {messages.length > 0 && messages.map((item, index) => <Message key={index}
-                                                                                   message={item.message}
-                                                                                   photo={item.photo}
-                                                                                   userId={item.userId}
-                                                                                   userName={item.userName}/>)}
-                    <div ref={messagesEndRef}/>
-                </ul>
-                <div className={style.addMessage}>
-                    <Textarea callback={clickAddNewMessages}
-                              validationSchema={validationPostAndDialog}
-                    />
-                </div>
-            </div>
-
+      </ul>
+      <div className={style.message}>
+        <ul className={style.dialogItem} onScroll={onScrollHandler}>
+          {messages.length > 0 && messages.map(( item, index ) => <Message key={index}
+                                                                           message={item.message}
+                                                                           photo={item.photo}
+                                                                           userId={item.userId}
+                                                                           userName={item.userName}/>)}
+          <div ref={messagesEndRef}/>
+        </ul>
+        <div className={style.addMessage}>
+          <Textarea callback={clickAddNewMessages}
+                    validationSchema={validationPostAndDialog}
+          />
         </div>
-    );
+      </div>
+
+    </div>
+  );
 };
 
 
---
+++ src/redux/test/userThunkFetchUnFollow.test.ts
@@ -0,0 +1,71 @@
+import {fetchUnFollowUser, InitialStateUserPageType, setUserSubscription, unFollow} from "../usersReducer";
+import {api, ResponseType} from "../../api/api";
+
+
+jest.mock("../../api/api")
+
+const apiMock = api as jest.Mocked<typeof api>
+const res: ResponseType = {
+  resultCode: 0,
+  data: {},
+  messages: []
+}
+
+
+let state: InitialStateUserPageType
+
+beforeEach(() => {
+  state = {
+    items: [{
+      name: "Anton",
+      id: 3,
+      uniqueUrlName: null,
+      photos: {
+        small: null,
+        large: null,
+      },
+      status: null,
+      followed: true
+    }],
+    totalCount: 0,
+    pageSize: 10,
+    currentPage: 1,
+    portionsNumber: 1,
+    error: null,
+    userSubscription: [] as Array<number>
+  }
+})
+
+test("un follow users", async () => {
+  let dispatchMock = jest.fn()
+  let getStateMock = jest.fn()
+  apiMock.unFollowUser.mockReturnValue(Promise.resolve(res))
+
+  let thunk = fetchUnFollowUser(3)
+
+  await thunk(dispatchMock, getStateMock, {})
+
+  expect(dispatchMock).toBeCalledTimes(3)
+
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, setUserSubscription(3, true))
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, unFollow(3))
+  expect(dispatchMock).toHaveBeenNthCalledWith(3, setUserSubscription(3, false))
+
+})
+
+test(" follow users", async () => {
+  let dispatchMock = jest.fn()
+  let getStateMock = jest.fn()
+  apiMock.unFollowUser.mockReturnValue(Promise.resolve(res))
+
+  let thunk = fetchUnFollowUser(2)
+
+  await thunk(dispatchMock, getStateMock, {})
+
+  expect(dispatchMock).toBeCalledTimes(3)
+
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, setUserSubscription(2, true))
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, unFollow(2))
+  expect(dispatchMock).toHaveBeenNthCalledWith(3, setUserSubscription(2, false))
+
+})
